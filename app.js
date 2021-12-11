eec9bc6c669f2ac4c382b920b80a6cc7577875a8
sidalieleuldj <sidali.eleuldj@gmail.com>
2021-12-11 11:25:58 +0100

    add condition

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 
-function generatesum (number1 , number2){
-    var sum = number1 + number2;
-    console.log(sum)
+function generatesum (n1 , n2){
+    if (typeof n1 == "number"){
+        if (typeof n2 == "number"){
+            var sum = n1 + n2;
+            console.log(sum)
+        }else { 
+            console.log("n2 must be a number")
+        }
+    }else{
+        console.log("n1 must be a number")
+    }
 }
 
-generatesum(20 , 3);
+generatesum(20 , 5);
