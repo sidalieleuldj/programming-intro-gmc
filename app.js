7ae55144b5781ddecc8aa4634341e1c66a24f688
sidalieleuldj <sidali.eleuldj@gmail.com>
2021-12-11 11:45:07 +0100

    add another if

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-
-function generatesum (n1 , n2){
-    if (typeof n1 == "number"){
-        if (typeof n2 == "number"){
-            var sum = n1 + n2;
-            console.log(sum)
-        }else { 
-            console.log("n2 must be a number")
-        }
-    }else{
-        console.log("n1 must be a number")
+function generatesum(n1, n2, n3) {
+  if (typeof n1 == "number") {
+    if (typeof n2 == "number") {
+      if (typeof n3 == "number") {
+        var sum = n1 + n2 + n3;
+        console.log(sum);
+      } else {
+        console.log("n3 is not number");
+      }
+    } else {
+      console.log("n2 must be a number");
     }
+  } else {
+    console.log("n1 must be a number");
+  }
 }
 
-generatesum(20 , 5);
+generatesum(20, 5 , 5);
